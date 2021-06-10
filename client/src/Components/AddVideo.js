360df68048b38e454382646c2aeb6a46ac25f9f2
Fatimohkuforiji1 <fatimohkuforiji@yahoo.com>
2021-06-10 22:27:18 +0100

    rendered the fetch API

--- client/src/Components/AddVideo.js
+++ client/src/Components/AddVideo.js
@@ -2,12 +2,35 @@ import React, { useState } from "react";
 import AddVideoSearch from "./AddVideoSearch";
 import VideoButton from "./videoButton.js";
 
+
+//  fetch("http://127.0.0.1:5000")
+//    .then((response) => response.json())
+//    .then((data) => {
+//      console.log(data);
+//      setVideoSearch(data);
+//    })
+//    .catch((error) => alert("Refresh The page, something went wrong"));
+
 const AddVideo = (props) => {
   const [hidden, setHidden] = useState(true);
   const [videoTitle, setVideoTitle] = useState("");
   const [videoURL, setVideoURL] = useState("");
  
+const handleAddVideo = (videoData) => {
+  const URLValidate =
+    /(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9_-]+)/;
+  if (videoData.title.trim().length !== 0 && videoData.url.match(URLValidate)) {
+    props.setVideoSearch([videoData, ...props.videoSearch]);
 
+    // .then((response) => response.json())
+  }
+  // else {
+  //   alert("Enter valid URL");
+  // }
+}
+  // modified: src / Components / AddVideo.js;
+  // modified: src / Components / AllVideoFiles.js;
+  // modified: src / Components / VideoGrid.js;
   const showHideForm = (event) => {
     event.preventDefault();
     const caption = event.target.textContent;
@@ -20,21 +43,34 @@ const AddVideo = (props) => {
     }
   };
 
+  const rating = 0;
   const collectVideoData = (event) => {
     event.preventDefault();
-    const videoData = { title: videoTitle, url: videoURL };
-    props.handleAddVideo(videoData);
+    const videoData = { title: videoTitle, url: videoURL, rating: rating };
+   
+    handleAddVideo(videoData);
     setVideoTitle("");
     setVideoURL("");
+
+    fetch("http://127.0.0.1:5000", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(videoData),
+    });
+
+    console.log(videoData);
   };
 
   return (
     <div>
-      {/* </div><div className="addVideo "> */}
       <form>
-        {hidden ? (
-          <button onClick={showHideForm}>Add Video</button>
-        ) : (
+      <h4>Hello</h4>
+        { hidden ? 
+        ( <button onClick={showHideForm}>Add Video</button>
+        ) :(
           <div className=" addVideoDisplay ">
             <p>Add Video</p>
             <div>
@@ -61,9 +97,8 @@ const AddVideo = (props) => {
             </div>
           </div>
         )}
-        {/* </div> */}
+      
       </form>
-      {/* </div> */}
       <AddVideoSearch handleVideoSearch={props.handleVideoSearch} />
       <VideoButton videoBtn={props.videoBtn} handleVideoBtn={props.handleVideoBtn} />
     </div>
